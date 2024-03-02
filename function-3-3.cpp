46794c58974228041188a53f23465b1ffb6db5c8
tuananh120534 <ndtanh.2005@gmail.com>
2024-03-02 18:50:39 +1100

    prac1

--- function-3-3.cpp
+++ function-3-3.cpp
@@ -2,19 +2,19 @@
 #include <unordered_map>
 
 double weighted_average(int array[], int n) {
-
     if (n < 1) {
         return 0.0;
     }
 
     std::unordered_map<int, int> frequencyMap;
+
     for (int i = 0; i < n; i++) {
         frequencyMap[array[i]]++;
     }
     double sum = 0.0;
     for (const auto& entry : frequencyMap) {
-        sum += static_cast<double>(entry.first * entry.second) / n;
+        sum += static_cast<double>(entry.first * entry.second);
     }
 
-    return sum;
+    return sum / n;
 }
